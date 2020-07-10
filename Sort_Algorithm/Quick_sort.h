d74f06c3981984e390acc1c42a18f23a04c964dc
QuenKar <47681251+QuenKar@users.noreply.github.com>
2020-07-10 08:38:50 +0800

    Update Quick_sort.h

--- Sort_Algorithm/Quick_sort.h
+++ Sort_Algorithm/Quick_sort.h
@@ -25,19 +25,3 @@ void qsort(int a[], int left, int right)
     if (i < right)
         qsort(a, i, right);
 }
-
-int main()
-{
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    qsort(a, 0, n - 1);
-    for (int i = 0; i < n; i++)
-    {
-        if (i != 0)
-            cout << ' ';
-        cout << a[i];
-    }
-    return 0;
-}
